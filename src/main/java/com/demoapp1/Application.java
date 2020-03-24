ced9d43b94a809a776b996fef0ef308a3abb9e7b
sekharbabu4patnam <babuthetrainer@gmail.com>
2020-03-24 07:55:17 +0530

    demo commit-1 from local to trigger jenkins job
    Signed-off-by: sekharbabu4patnam <babuthetrainer@gmail.com>

--- src/main/java/com/demoapp1/Application.java
+++ src/main/java/com/demoapp1/Application.java
@@ -2,6 +2,6 @@ package com.demoapp1;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Demo Application....!");
+        System.out.println("Azure Demo Application....!");
     }
 }
